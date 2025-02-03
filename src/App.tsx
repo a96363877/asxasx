a6ab8ae552297861b1becfdd273f00e6b5c097a4
a96363877 <a96363877@gmail.com>
2025-02-03 08:18:27 -0800

    as

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { Loader } from './loader';
 
 function App() {
 
-  const [currantPage, setCurrantPage] = useState(3);
+  const [currantPage, setCurrantPage] = useState(1);
   const [isLoading, setisloading] = useState(false);
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
