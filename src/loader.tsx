87a67935a73519ccacc18e33d3398cb52d6b9803
a96363877 <a96363877@gmail.com>
2025-02-05 05:42:11 -0800

    er

--- src/loader.tsx
+++ src/loader.tsx
@@ -1,7 +1,7 @@
 
 export const Loader = (props:{show:boolean}) => {
     return (<div style={{display:props.show?"flex":"none",position:'fixed',
-    flexDirection:"column",textAlign:'center',justifyItems:"center",justifyContent:'center',top:'45%',bottom:'auto',left:0,right:0,zIndex:1}}>
+    flexDirection:"column",textAlign:'center',justifyItems:"center",justifyContent:'center',top:'40%',bottom:'auto',left:0,right:0,zIndex:1}}>
         <img
         style={{position:'fixed',left:'45%',top:'40%'}}
         className="w-full m-auto py-16"
